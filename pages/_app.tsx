c0dff8cc952fc943e715c34f4c8d864c976367ef
mahad shbeeb <mahad.shbeeb@wardlin.com>
2025-02-02 00:24:23 +0300

    adding a descriptive readme.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import MainLayout from "@/components/layout/MainLayout";
 import "@/styles/globals.css";
-import { Box, CssBaseline } from "@mui/material";
+import { Box, CssBaseline, styled } from "@mui/material";
 import type { AppProps } from "next/app";
+import { MaterialDesignContent, SnackbarProvider } from "notistack";
+
+const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
+  "&.notistack-MuiContent-success": {
+    backgroundColor: "green",
+    color: "#fff",
+  },
+  "&.notistack-MuiContent-error": {
+    backgroundColor: "#D84040",
+    color: "#fff",
+  },
+}));
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,13 +21,20 @@ export default function App({ Component, pageProps }: AppProps) {
       sx={{
         minHeight: "100vh",
         backgroundColor: "#fff",
-        // color: "#333",
       }}
     >
-      <CssBaseline />
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <SnackbarProvider
+        Components={{
+          success: StyledMaterialDesignContent,
+          error: StyledMaterialDesignContent,
+        }}
+        maxSnack={3}
+      >
+        <CssBaseline />
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </SnackbarProvider>
     </Box>
   );
 }
